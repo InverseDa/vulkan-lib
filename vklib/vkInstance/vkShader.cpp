381a8e38595a132a3d24a246e3c6bf38747d1fb1
InverseDa <inverse_boom@outlook.com>
2024-02-19 01:53:35 +0800

    feats: shader module

---
+++ vklib/vkInstance/vkShader.cpp
@@ -0,0 +1,67 @@
+#include "vkInstance/vkShader.h"
+
+namespace Vulkan {
+
+ShaderModule::ShaderModule(VkShaderModuleCreateInfo& createInfo) {
+    Create(createInfo);
+}
+
+ShaderModule::ShaderModule(const char* filePath) {
+    Create(filePath);
+}
+
+ShaderModule::ShaderModule(size_t codeSize, const uint32_t* pCode) {
+    Create(codeSize, pCode);
+}
+
+ShaderModule::ShaderModule(ShaderModule&& other) noexcept {
+    MoveHandle;
+}
+
+ShaderModule::~ShaderModule() {
+    DestroyHandleBy(vkDestroyShaderModule);
+}
+
+VkPipelineShaderStageCreateInfo ShaderModule::GetStageCreateInfo(VkShaderStageFlagBits stage, const char* entry) const {
+    return {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
+        .pNext = nullptr,
+        .flags = 0,
+        .stage = stage,
+        .pName = entry,
+        .pSpecializationInfo = nullptr,
+    };
+}
+
+ResultType ShaderModule::Create(VkShaderModuleCreateInfo& createInfo) {
+    createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+    VkResult result = vkCreateShaderModule(GraphicsBase::GetInstance().GetDevice(), &createInfo, nullptr, &handle);
+    if (result) {
+        outStream << std::format("[ShaderModule][ERROR] Failed to create a shader module! Error: {}({})\n", string_VkResult(result), int32_t(result));
+    }
+    return result;
+}
+
+ResultType ShaderModule::Create(const char* filePath) {
+    std::ifstream file(filePath, std::ios::ate | std::ios::binary);
+    if (!file) {
+        outStream << std::format("[ShaderModule][ERROR] Failed to open file: {}\n", filePath);
+        return VK_RESULT_MAX_ENUM;
+    }
+    size_t fileSize = file.tellg();
+    std::vector<uint32_t> buffer(fileSize / 4);
+    file.seekg(0);
+    file.read(reinterpret_cast<char*>(buffer.data()), fileSize);
+    file.close();
+    return Create(fileSize, buffer.data());
+}
+
+ResultType ShaderModule::Create(size_t codeSize, const uint32_t* pCode) {
+    VkShaderModuleCreateInfo createInfo = {
+        .codeSize = codeSize,
+        .pCode = pCode,
+    };
+    return Create(createInfo);
+}
+
+} // namespace Vulkan
