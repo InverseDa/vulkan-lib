02235db148061b1953d348f4b9af87ee356a1b50
InverseDa <inverse_boom@outlook.com>
2024-02-19 02:37:00 +0800

    feats: vk pipeline

---
+++ vklib/vkInstance/vkPipeline.cpp
@@ -0,0 +1,38 @@
+#include "vkInstance/vkPipeline.h"
+
+namespace Vulkan {
+Pipeline::Pipeline(VkGraphicsPipelineCreateInfo& createInfo) {
+    Create(createInfo);
+}
+
+Pipeline::Pipeline(VkComputePipelineCreateInfo& createInfo) {
+    Create(createInfo);
+}
+
+Pipeline::Pipeline(Pipeline&& other) noexcept {
+    MoveHandle;
+}
+
+Pipeline::~Pipeline() {
+    DestroyHandleBy(vkDestroyPipeline);
+}
+
+ResultType Pipeline::Create(VkGraphicsPipelineCreateInfo& createInfo) {
+    createInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+    VkResult result = vkCreateGraphicsPipelines(GraphicsBase::GetInstance().GetDevice(), VK_NULL_HANDLE, 1, &createInfo, nullptr, &handle);
+    if (result) {
+        outStream << std::format("[Pipeline][ERROR] Failed to create a graphics pipeline! Error: {}({})\n", string_VkResult(result), int32_t(result));
+    }
+    return result;
+}
+
+ResultType Pipeline::Create(VkComputePipelineCreateInfo& createInfo) {
+    createInfo.sType = VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO;
+    VkResult result = vkCreateComputePipelines(GraphicsBase::GetInstance().GetDevice(), VK_NULL_HANDLE, 1, &createInfo, nullptr, &handle);
+    if (result) {
+        outStream << std::format("[Pipeline][ERROR] Failed to create a compute pipeline! Error: {}({})\n", string_VkResult(result), int32_t(result));
+    }
+    return result;
+}
+
+} // namespace Vulkan
