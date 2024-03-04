b510b060c6990936dcbede093cc73a08ec55a2ba
InverseDa <inverse_boom@outlook.com>
2024-03-04 14:31:23 +0800

    dev branch init

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,9 +1,4 @@
-#include "app/app.h"
 
 int main() {
-    Vulkan::Application app(800, 600, false, true, true, "Vulkan Application");
-    return app.Run([&](){
-        vkCmdBindPipeline(app.GetCommandBuffer(), VK_PIPELINE_BIND_POINT_GRAPHICS, app.GetPipeline());
-        vkCmdDraw(app.GetCommandBuffer(), 3, 1, 0, 0);
-    });
+
 }
--- vklib/type/ostream.h
+++
@@ -1,8 +0,0 @@
-#ifndef VULKAN_LIB_OSTREAM_H
-#define VULKAN_LIB_OSTREAM_H
-
-#include <iostream>
-
-inline auto& outStream = std::cout;
-
-#endif // VULKAN_LIB_OSTREAM_H
