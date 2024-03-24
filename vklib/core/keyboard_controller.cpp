6f9fb9e20d9fe143ca0ebf3919d564307fb0ca68
InverseDa <inverse_boom@outlook.com>
2024-03-24 13:42:56 +0800

    feats: change sdl2 to glfw

--- vklib/core/keyboard_controller.cpp
+++ vklib/core/keyboard_controller.cpp
@@ -14,7 +14,7 @@ void KeyboardMovementController::MoveInPlaneXZ(GLFWwindow* window, float dt, ida
         rotate.x -= 1.f;
 
     if (glm::dot(rotate, rotate) > std::numeric_limits<float>::epsilon()) {
-        gameObject.transform.rotation = lookSpeed * dt * glm::normalize(rotate);
+        gameObject.transform.rotation += lookSpeed * dt * glm::normalize(rotate);
     }
 
     gameObject.transform.rotation.x = glm::clamp(gameObject.transform.rotation.x, -1.5f, 1.5f);
